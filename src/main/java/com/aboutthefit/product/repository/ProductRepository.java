5594ce353ff3d4deaafce58bf8bc4b09e952288d
nisha-tamta <nisha.tamta199@gmail.com>
2021-07-03 22:05:52 +0530

    Removed unnecessary imports

--- src/main/java/com/aboutthefit/product/repository/ProductRepository.java
+++ src/main/java/com/aboutthefit/product/repository/ProductRepository.java
@@ -4,7 +4,6 @@
 package com.aboutthefit.product.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
