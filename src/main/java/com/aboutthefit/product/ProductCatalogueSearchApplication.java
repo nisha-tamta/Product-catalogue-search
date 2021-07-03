4b53c176b4644c029fd81be98d78fc33e12f7073
nisha-tamta <nisha.tamta199@gmail.com>
2021-07-03 22:09:30 +0530

    Added author

--- src/main/java/com/aboutthefit/product/ProductCatalogueSearchApplication.java
+++ src/main/java/com/aboutthefit/product/ProductCatalogueSearchApplication.java
@@ -3,6 +3,10 @@ package com.aboutthefit.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author nisha
+ *
+ */
 @SpringBootApplication
 public class ProductCatalogueSearchApplication {
 
